98bf54caf045b12379dbadcb5a63e0e2dc0f84b5
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-08-07 23:56:27 +0530

    client side made

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 
 // Load all environment variables 
-require('dotenv0').config()
+require('dotenv').config()
 
 //setting up express
 const express=require ('express');
-const app= express
+const app= express();
 
 // read all JSON data that gets sent to the server
 
 app.use(express.json())
 
+// for client side
+app.use(express.static("public"))
+
 // Key Value pair of the items in the store
 const storeItems = new Map([
-    [1,{priceInPaise:10000,name="Binod"}],
-    [2,{priceInPaise:20000,name="Father of Binod"}],
+    [1,{priceInPaise:10000,name:"Binod"}],
+    [2,{priceInPaise:20000,name:"Father of Binod"}],
 ])
 
 // Setting up Stripe
-const stripe=require('stripe'(process.env.STRIPE_KEY)
+const stripe=require('stripe')(process.env.STRIPE_KEY)
 
 app.listen(3000)    
